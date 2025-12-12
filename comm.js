d64e355e0a78118eea5c9e05acefe79c89e086d3
VIGNESH.V <vicky.v20040327@gmail.com>
2025-12-12 13:28:14 +0530

    This is Commend line program in JavaScript

---
+++ comm.js
@@ -0,0 +1,15 @@
+1)
+console.log("hi this is comment program")
+// Single line Comment 
+console.log("Single line Comment is successfully completed")
+
+2)
+/*
+console.log("This is Multi Line comment program");
+Using /* and */
+*/
+
+3)
+Using Keyboard  for Comment line use "Ctrl+/" 
+Rule :
+    //Select particular line or multi line using above keys
