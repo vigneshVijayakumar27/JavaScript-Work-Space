29069aaa716bde7cbd749aa088393e0035a9ee2f
VIGNESH.V <vicky.v20040327@gmail.com>
2025-12-12 14:02:07 +0530

    Variable Declarations Using JavaScript

---
+++ Variables.js
@@ -0,0 +1,32 @@
+// Variable Declaration .
+let bookname;
+let Count;
+
+// Variable Initialization.
+a=10;
+b="hi";
+c=1.2;
+
+let d="First", e="Second", f="Third";
+
+let g="First",
+    h="Second",
+    i="third";
+
+// Old Declaration Method.
+var j=100;
+
+// Variable assigning to another variable and alterations.
+let k="Hi";
+k="Hello";
+let l=k;
+
+/* 
+    Rules:
+       - Variable names all are unique name
+       - Special charecters allowed onley $ , _ , Numbers. 
+       - Kewords not Allowed 
+       - Case Sencitive
+            Ex. var name="Mani";
+                var Name="Kumar";
+*/
