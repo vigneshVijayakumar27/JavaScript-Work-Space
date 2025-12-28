b65e0685c8e27522ffaf8d4af41c175db3ab4f34
VIGNESH.V <vicky.v20040327@gmail.com>
2025-12-28 12:23:48 +0530

    String Data Type in JavaScript (Org)

---
+++ String.js
@@ -0,0 +1,17 @@
+// String Data Type.
+// It using Single quotes and double quotes.
+
+let name="Vignesh.V";
+let intro="i am 'Vignesh' from Salem ";
+let address='Nalikkal Patti in "Salem"';
+console.log(name);
+console.log(intro);
+console.log(address+"\n");
+
+// Another Ex.
+
+let Self=`This is a normal "String" line`;
+console.log(Self+"\n"); 
+
+let embed=`My name is ${name}`;
+console.log(embed);  
