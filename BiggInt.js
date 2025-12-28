45514ac6ac0ba85206005706a14966687f9a647e
VIGNESH.V <vicky.v20040327@gmail.com>
2025-12-28 12:22:54 +0530

    BigInt in JavaScript

---
+++ BiggInt.js
@@ -0,0 +1,15 @@
+// BigInt
+// We should assign 'n' in the last nearby of numbers.
+
+let Big=9007199254740991n;
+console.log(Big+1n);
+console.log(Big+2n);
+console.log(Big+3n);
+
+console.log(typeof Big);
+
+// Ex.
+
+let PeriyaNumber=12345678910n;
+PeriyaNumber=PeriyaNumber+2025n;
+console.log(PeriyaNumber);
